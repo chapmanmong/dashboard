0d28153b345b9635b2b2d877989cf5eeadb19b55
Chapman Mong <chapmanmong@gmail.com>
2021-08-11 14:00:06 +1000

    started with geolocation but having troubles with extracting longitute and lattitude

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
- class DigitalClock {
-     constructor(element) {
-         this.element = element;
-     }
+const KEY = "AIzaSyBrI2TLDVgMq3uZlOdd2nTk1TKXW2jsLzM"
 
-     start(){
-         this.update();
-         setInterval(() => {
-             this.update();
-         }, 500);
-     }
+class DigitalClock {
+    constructor(element) {
+        this.element = element;
+    }
 
-     update(){
+    start() {
+        this.update();
+        setInterval(() => {
+            this.update();
+        }, 500);
+    }
+
+    update() {
         const parts = this.getTimeParts();
         const minutesFormatted = parts.minute.toString().padStart(2, "0");
         const timeFormatted = `${parts.hour}:${minutesFormatted}`
@@ -18,20 +20,45 @@
 
         this.element.querySelector(".clock-time").textContent = timeFormatted;
         this.element.querySelector(".clock-ampm").textContent = amPM;
-        console.log(timeFormatted);
     };
 
-     getTimeParts(){
-         const now = new Date();
-         return {
+    getTimeParts() {
+        const now = new Date();
+        return {
             hour: now.getHours() % 12 || 12,
             minute: now.getMinutes(),
             isAM: now.getHours() < 12
-         };
-     }
- }
+        };
+    }
+}
 
- const clockElement = document.querySelector(".clock");
- const clockObject = new DigitalClock(clockElement);
+const clockElement = document.querySelector(".clock");
+const clockObject = new DigitalClock(clockElement);
 
 clockObject.start()
+
+if ('geolocation' in navigator) {
+    console.log("geolocation available");
+} else {
+    console.log("geolocation not available");
+}
+
+var long;
+var lat;
+
+navigator.geolocation.getCurrentPosition((position) => {
+    long = position.coords.longitude;
+    lat = position.coords.latitude;
+});
+
+console.log(long, lat);
+
+query = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=${KEY}`;
+console.log(query);
+
+fetch(query)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => console.warn(err.message));
